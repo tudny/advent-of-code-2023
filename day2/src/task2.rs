b02ad4d1e9367da96d65e5f182419cb23c8c19aa
Aleksander Tudruj <aleksanderwt@gmail.com>
2023-12-02 19:01:29 +0100

    day2 faster, fmt, clippy

--- day2/src/task2.rs
+++ day2/src/task2.rs
@@ -1,79 +1,67 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::cmp::max;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use regex::Regex;
-use lazy_static::lazy_static;
 
 lazy_static! {
     static ref LINE_RE: Regex = Regex::new(r"Game (?<id>[0-9]+): (?<draws>.*)").unwrap();
     static ref BALL_RE: Regex = Regex::new(r"(?<number>[0-9]+) (?<color>[a-z]+)").unwrap();
 }
 
-const GREEN: &str = "green";
-const BLUE: &str = "blue";
-const RED: &str = "red";
+const ACCEPTABLE_COLORS: [&str; 3] = ["green", "blue", "red"];
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path> {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn validate_draw(draw: &str) -> Option<(usize, usize, usize)> {
-    let mut green = 0;
-    let mut blue = 0;
-    let mut red = 0;
+fn validate_draw(draw: &str) -> Option<HashMap<&str, usize>> {
+    let mut result = HashMap::from(ACCEPTABLE_COLORS.map(|c| (c, 0)));
     for balls in draw.split(", ") {
-        let cap = BALL_RE.captures(&balls)?;
+        let cap = BALL_RE.captures(balls)?;
         let color = &cap["color"];
         let number = &cap["number"].parse::<usize>().ok()?;
-        match color {
-            GREEN => { green += number }
-            BLUE => { blue += number }
-            RED => { red += number }
-            _ => {}
+        if let Some(current) = result.get_mut(color) {
+            *current += number;
         }
     }
 
-    Some((green, blue, red))
+    Some(result)
 }
 
-fn check_single_line(line: String) -> Option<usize> {
-    let cap = LINE_RE.captures(&line)?;
-    let mut green = 0;
-    let mut blue = 0;
-    let mut red = 0;
+fn check_single_line(line: &str) -> Option<usize> {
+    let cap = LINE_RE.captures(line)?;
 
-    let update = |old: &mut usize, new: usize| {
-        if *old < new {
-            *old = new;
+    let mut current_scores = HashMap::from(ACCEPTABLE_COLORS.map(|c| (c, 0)));
+
+    for draw_scores in cap["draws"].split("; ").flat_map(validate_draw) {
+        for (color, current_score) in current_scores.iter_mut() {
+            let score = draw_scores.get(color)?;
+            *current_score = max(*current_score, *score);
         }
-    };
-
-    for (g, b, r) in cap["draws"].split("; ").map(validate_draw).flatten() {
-        update(&mut green, g);
-        update(&mut blue, b);
-        update(&mut red, r);
     }
 
-    Some(green * blue * red)
+    Some(current_scores.values().product())
 }
 
 fn solve(filename: &str) -> usize {
     let mut sum = 0;
 
     if let Ok(lines) = read_lines(filename) {
-        for line in lines {
-            if let Ok(line) = line {
-                sum += check_single_line(line).unwrap_or_else(|| 0);
-            }
+        for line in lines.flatten() {
+            sum += check_single_line(&line).unwrap_or(0);
         }
     }
 
-    return sum;
+    sum
 }
-
 
 fn main() {
     const FILENAME: &str = "input.txt";
